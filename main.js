15b2e46140e838ed055e4c845eab8129763ea596
dalifyyy <d4lified@gmail.com>
2026-07-29 20:26:57 +0800

    added some stuff

---
+++ main.js
@@ -0,0 +1,11 @@
+import * as Tone from "tone";
+
+var github_repo = ""
+
+function set_github_repo(repo_link) {
+  github_repo = repo_link;
+}
+
+function create_tool() {
+  var synth = new Tone.synth().toDestination();
+}
