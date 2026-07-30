2292a14a0cd67f487ff9aa5f0745a2abdbfc3219
dalifyyy <d4lified@gmail.com>
2026-07-30 11:25:37 +0800

    feat: some stuff

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-import * as Tone from "tone";
-import { execSync } from 'child_process';
+import * as Tone  from "tone";
 
-const log = execSync('git log --numstat --pretty=format:"%H|%an|%ad|%s"', {
-  cwd: '/path/to/repo',
-  encoding: 'utf-8'
-});
-var github_repo = ""
+var log
+var github_repo = "";
 
 function set_github_repo(repo_link) {
   github_repo = repo_link;
